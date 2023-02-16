b54a3f44e48cb0f85c0703e68567fa4d9bbaf4f5
eMobilis <elikibetael4@gmail.com>
2023-02-16 09:18:58 +0300

    new deeejango changes

--- deeejangoProject/urls.py
+++ deeejangoProject/urls.py
@@ -15,15 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about')
+    path('services/', views.services, name='services')
+
 ]
-
-git init
-
-
-g
-
-
-
---
+++ deeejangoProject/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+
+def about(request):
+    return render(request, "about.html")
+def services(request):
+    return render(request, "services.html")
+def contact_us(request):
+    return render(request, "contact us.html")
+
